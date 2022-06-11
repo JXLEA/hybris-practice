3b3ac8b249d4c7180e8648a827ff972f471e691b
JXlea <alexeyy.nosov@gmail.com>
2022-06-11 12:49:20 +0300

    Initial commit

---
+++ training/traininginitialdata/gensrc/com/epam/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jun 8, 2022, 4:43:21 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.epam.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ training/trainingtest/gensrc/com/epam/training/test/constants/GeneratedTrainingTestConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jun 8, 2022, 4:43:21 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.epam.training.test.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingTestConstants
+{
+	public static final String EXTENSIONNAME = "trainingtest";
+	
+	protected GeneratedTrainingTestConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
