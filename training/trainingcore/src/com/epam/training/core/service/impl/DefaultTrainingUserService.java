f9d87f7601045a59e740fc93e4efc8aca5aa66ba
JXlea <alexeyy.nosov@gmail.com>
2022-07-18 14:59:51 +0300

    impex | add data layer, fix tests

--- training/trainingcore/src/com/epam/training/core/service/impl/DefaultTrainingUserService.java
+++ training/trainingcore/src/com/epam/training/core/service/impl/DefaultTrainingUserService.java
@@ -1,29 +1,26 @@
 package com.epam.training.core.service.impl;
 
+import com.epam.training.core.dao.TestUserDao;
 import com.epam.training.core.service.TrainingUserService;
 import de.hybris.platform.core.model.user.UserModel;
-import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
-import de.hybris.platform.servicelayer.search.FlexibleSearchService;
 import de.hybris.platform.servicelayer.user.impl.DefaultUserService;
 
 import java.util.List;
 
 public class DefaultTrainingUserService extends DefaultUserService implements TrainingUserService {
 
-    private FlexibleSearchService flexibleSearchService;
+    private TestUserDao userDao;
 
     @Override
     public List<UserModel> getUsersWithoutAddress() {
-        var query = "SELECT {pk} FROM {User as u} WHERE {u.pk} NOT IN ({{SELECT {a.owner} FROM {Address as a}}})";
-        var flxSrchQuery = new FlexibleSearchQuery(query);
-        return flexibleSearchService.<UserModel>search(flxSrchQuery).getResult();
+        return userDao.getUsersWithoutAddress();
     }
 
-    public FlexibleSearchService getFlexibleSearchService() {
-        return flexibleSearchService;
+    public TestUserDao getUserDao() {
+        return userDao;
     }
 
-    public void setFlexibleSearchService(FlexibleSearchService flexibleSearchService) {
-        this.flexibleSearchService = flexibleSearchService;
+    public void setUserDao(TestUserDao userDao) {
+        this.userDao = userDao;
     }
 }
